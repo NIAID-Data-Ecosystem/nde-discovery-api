a57fe9dc3a4d0b48c78b4bb5773ee84d5bb9fed9
Dylan Welzel <dylanwelzel@gmail.com>
2022-04-29 16:13:32 -0700

    sidebar resize

--- web-app/src/components/SourcesUI/index.js
+++ web-app/src/components/SourcesUI/index.js
@@ -2,7 +2,7 @@ import SourcesHeader from "../SourcesHeader";
 import SourcesMain from "../SourcesMain";
 import { useState, useEffect } from 'react'
 import SourcesSidebar from "../SourcesSidebar";
-
+import './sourcessidebar.css'
 
 const SourcesUI = () => {
 
@@ -32,7 +32,7 @@ const SourcesUI = () => {
                 <>
                     <SourcesHeader />
                     <div className="min-h-screen flex flex-row">
-                        <div className="flex flex-col bg-gray-100   text-white w-6/12 mobile:hidden">
+                        <div className="flex flex-col bg-gray-100 sidebar-container  text-white w-6/12 mobile:hidden">
                             <ul className="flex flex-col py-4 sticky top-0  divide-y divide-y divide-gray-400">
                                 <SourcesSidebar sourceData={sourceData} />
                             </ul>
