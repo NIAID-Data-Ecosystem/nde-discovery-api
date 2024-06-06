dddfc76f890b7ede8e8ea836771ccd994298a5d3
Dylan Welzel <dylanwelzel@gmail.com>
2024-06-06 09:53:27 -0700

    update lincs name

--- nde-web/handlers.py
+++ nde-web/handlers.py
@@ -701,7 +701,7 @@ class NDESourceHandler(MetadataSourceHandler):
                 "abstract": "The Library of Integrated Network-Based Cellular Signatures (LINCS) Data Portal is an NIH supported repository that includes gene expression and other cellular processes data.",
                 "description": "The BD2K-LINCS DCIC is comprised of four major components: Integrated Knowledge Environment (IKE), Data Science Research (DSR), Community Training and Outreach (CTO) and Consortium Coordination and Administration (CCA). The Center is constructing a high-capacity scalable integrated knowledge environment enabling federated access, intuitive querying and integrative analysis and visualization across all LINCS resources and many additional external data types from other relevant resources. The Center’s data science research projects are aimed at addressing various data integration and intracellular molecular regulatory network challenges. The Center aims to develop: 1) methods to connect cellular and organismal phenotypes with molecular cellular signatures, and 2) novel data visualization methods for dynamically interacting with large-genomics and proteomics datasets.",
                 "identifier": "LINCS",
-                "name": "BD2K-LINCS DCIC",
+                "name": "LINCS",
                 "schema": {
                     "centerdatasetid": "url",
                     "funding": "funding",
