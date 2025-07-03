97af86c13a2de9d32251ff012f2bd6bd0869bbb1
Dylan Welzel <dylanwelzel@gmail.com>
2025-07-03 09:55:58 -0700

    add lineage parameter and logic

--- nde-web/pipeline.py
+++ nde-web/pipeline.py
@@ -2,6 +2,36 @@ from biothings.web.query import ESQueryBuilder, ESResultFormatter
 from elasticsearch_dsl import A, Q, Search
 
 
+def transform_lineage_response(response):
+    facets = response.get('facets', {})
+    lineage = facets.get('lineage', {})
+    children = lineage.get('children_of_lineage', {})
+    taxon_ids_data = children.get('taxon_ids', {})
+    lineage_total = facets.get('lineage_total_count', {})
+
+    transformed = {
+        "lineage": {
+            "totalRecords": facets.get("lineage_doc_count", {}).get(
+                "doc_count"),
+            "totalLineageRecords": lineage_total.get("inner_filter", {}).get(
+                "to_parent", {}).get("doc_count"),
+            "children": {
+                "totalChildRecords": children.get("doc_count"),
+                "totalUniqueChildRecords": children.get("to_parent", {}).get(
+                    "doc_count"),
+                "childTaxonCounts": [
+                    {
+                        "taxonId": term.get("term"),
+                        "count": term.get("count"),
+                    }
+                    for term in taxon_ids_data.get("terms", [])
+                ]
+            }
+        }
+    }
+    return transformed
+
+
 class NDEQueryBuilder(ESQueryBuilder):
     # https://docs.biothings.io/en/latest/_modules/biothings/web/query/builder.html#ESQueryBuilder.default_string_query
 
@@ -14,6 +44,8 @@ class NDEQueryBuilder(ESQueryBuilder):
         if ":" in q or " AND " in q or " OR " in q:
             search = search.query("query_string", query=q,
                                   default_operator="AND", lenient=True)
+
+        # term search
         elif q.startswith('"') and q.endswith('"'):
             queries = [
                 # term query boosting
@@ -22,6 +54,7 @@ class NDEQueryBuilder(ESQueryBuilder):
                 # query string
                 Q("query_string", query=q, default_operator="AND", lenient=True),
             ]
+
             search = search.query("dis_max", queries=queries)
 
         # simple text search
@@ -40,7 +73,7 @@ class NDEQueryBuilder(ESQueryBuilder):
             Q("function_score", query=Q("query_string", query=q, default_operator="AND", lenient=True),
               script_score={"script": custom_function_script} if custom_function_script else None, boost_mode="replace"),
         ]
-
+        # check if q contains wildcards if not add wildcard query to every word
         if not ("*" in q or "?" in q):
             wc_query = Q(
                 "query_string",
@@ -98,7 +131,6 @@ class NDEQueryBuilder(ESQueryBuilder):
                 min_doc_count=1,
             )
             search.aggs.bucket("hist_dates", a)
-
         # apply suggester
         if options.suggester:
             phrase_suggester = {
@@ -135,7 +167,6 @@ class NDEQueryBuilder(ESQueryBuilder):
                                      "script": custom_function_script}, boost_mode="replace")
             search = search.query(function_score_query)
         else:
-            # Still boosting ResourceCatalogs
             functions = [
                 {"filter": {"term": {"@type": "ResourceCatalog"}}, "weight": 1000}
             ]
@@ -158,39 +189,139 @@ class NDEQueryBuilder(ESQueryBuilder):
             )
             search.aggs.bucket("multi_terms_agg", multi_terms_agg)
 
+        # hide _meta object
         if options.show_meta:
             search = search.source(includes=["*"], excludes=[])
         else:
             search = search.source(excludes=["_meta"])
+
+        # # spam filter
+        # spam_filter = Q(
+        #     "bool",
+        #     should=[
+        #         Q("bool", must=[Q("match", name="keto"),
+        #           Q("match", name="gummies")]),
+        #         Q("bool", must=[Q("match", description="keto"),
+        #           Q("match", description="gummies")]),
+        #     ],
+        #     minimum_should_match=1
+        # )
+        # search = search.exclude(spam_filter)
+
+        if options.get('lineage'):
+            lineage_taxon_id = options.get('lineage')
+
+            # Existing lineage aggregation
+            lineage_agg = A('nested', path='_meta.lineage')
+
+            children_of_lineage_filter = A(
+                'filter', term={'_meta.lineage.parent_taxon': lineage_taxon_id})
+
+            taxon_ids_terms = A('terms', field='_meta.lineage.taxon')
+            taxon_ids_terms.bucket('to_parent', A('reverse_nested'))
+
+            children_of_lineage_filter.bucket('to_parent', A('reverse_nested'))
+            children_of_lineage_filter.bucket('taxon_ids', taxon_ids_terms)
+
+            lineage_agg.bucket('children_of_lineage',
+                               children_of_lineage_filter)
+
+            search.aggs.bucket('lineage', lineage_agg)
+
+            # New aggregation for counting datasets based on species.identifier and infectiousAgent.identifier
+            # Since these fields are not nested, we can query them directly
+            lineage_taxon_filter = Q(
+                'bool',
+                should=[
+                    Q('term', **{'species.identifier': lineage_taxon_id}),
+                    Q('term', **
+                      {'infectiousAgent.identifier': lineage_taxon_id})
+                ],
+                minimum_should_match=1
+            )
+
+            # Create a filter aggregation using the above filter
+            lineage_doc_count_agg = A('filter', lineage_taxon_filter)
+
+            # Include this aggregation at the top level
+            search.aggs.bucket('lineage_doc_count', lineage_doc_count_agg)
+
+            # New aggregation for counting total lineage records
+            # based on _meta.lineage.taxon
+            lineage_total_filter = A('nested', path='_meta.lineage')
+            lineage_total_inner_filter = A(
+                'filter', term={'_meta.lineage.taxon': lineage_taxon_id})
+            lineage_total_inner_filter.bucket('to_parent', A('reverse_nested'))
+            lineage_total_filter.bucket(
+                'inner_filter', lineage_total_inner_filter)
+            search.aggs.bucket('lineage_total_count', lineage_total_filter)
 
         return super().apply_extras(search, options)
 
 
 class NDEFormatter(ESResultFormatter):
     def transform_aggs(self, res):
-        for facet in res:
-            res[facet]["_type"] = "terms"  # a type of ES Bucket Aggs
-            res[facet]["terms"] = res[facet].pop("buckets")
-            # Quick fix b/c hist aggregation is not a nested aggs and does not contain sum_other_doc_count and doc_count_error_upper_bound
-            res[facet]["other"] = res[facet].pop("sum_other_doc_count", 0)
-            res[facet]["missing"] = res[facet].pop(
-                "doc_count_error_upper_bound", 0)
+        def transform_agg(agg_res):
+            if 'buckets' in agg_res:
+                agg_res['_type'] = 'terms'
+                agg_res['terms'] = agg_res.pop('buckets')
+                agg_res['other'] = agg_res.pop('sum_other_doc_count', 0)
+                agg_res['missing'] = agg_res.pop(
+                    'doc_count_error_upper_bound', 0)
+                count = 0
+                for bucket in agg_res['terms']:
+                    bucket['count'] = bucket.pop('doc_count')
+                    bucket['term'] = bucket.pop('key')
+                    if 'key_as_string' in bucket:
+                        bucket['term'] = bucket.pop('key_as_string')
+                    count += bucket['count']
+                    for k in list(bucket.keys()):
+                        if isinstance(bucket[k], dict):
+                            transform_agg(bucket[k])
+                agg_res['total'] = count
+            else:
+                for k in list(agg_res.keys()):
+                    if isinstance(agg_res[k], dict):
+                        transform_agg(agg_res[k])
+        transform_agg(res)
 
-            count = 0
-
-            for bucket in res[facet]["terms"]:
-                bucket["count"] = bucket.pop("doc_count")
-                bucket["term"] = bucket.pop("key")
-                if "key_as_string" in bucket:
-                    bucket["term"] = bucket.pop("key_as_string")
-                count += bucket["count"]
-
-                # nested aggs
-                for agg_k in list(bucket.keys()):
-                    if isinstance(bucket[agg_k], dict):
-                        bucket.update(self.transform_aggs(
-                            dict({agg_k: bucket[agg_k]})))
-
-            res[facet]["total"] = count
+        # If lineage aggregations are present in the facets,
+        # apply our transformation
+        if 'facets' in res and 'lineage' in res['facets']:
+            raw_lineage = res['facets']['lineage']
+            raw_lineage_doc_count = res['facets'].get('lineage_doc_count', {})
+            raw_lineage_total_count = res['facets'].get(
+                'lineage_total_count', {})
+            # Prepare a temporary response structure for the transformer
+            lineage_response = {
+                "facets": {
+                    "lineage": raw_lineage,
+                    "lineage_doc_count": raw_lineage_doc_count,
+                    "lineage_total_count": raw_lineage_total_count
+                }
+            }
+            transformed_lineage = transform_lineage_response(lineage_response)
+            # Replace the raw lineage aggregation with our
+            # transformed structure
+            res['facets']['lineage'] = transformed_lineage['lineage']
+            res['facets'].pop('lineage_doc_count', None)
+            res['facets'].pop('lineage_total_count', None)
+        elif 'lineage' in res:
+            # For responses where lineage is at the top level
+            # rather than under 'facets'
+            raw_lineage = res['lineage']
+            raw_lineage_doc_count = res.get('lineage_doc_count', {})
+            raw_lineage_total_count = res.get('lineage_total_count', {})
+            lineage_response = {
+                "facets": {
+                    "lineage": raw_lineage,
+                    "lineage_doc_count": raw_lineage_doc_count,
+                    "lineage_total_count": raw_lineage_total_count
+                }
+            }
+            transformed_lineage = transform_lineage_response(lineage_response)
+            res['lineage'] = transformed_lineage['lineage']
+            res.pop('lineage_doc_count', None)
+            res.pop('lineage_total_count', None)
 
         return res
