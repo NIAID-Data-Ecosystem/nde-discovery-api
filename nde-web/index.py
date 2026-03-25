5e25ed46dbf31cbb4d0d0179f46a50b543fa1f3d
Dylan Welzel <dylanwelzel@gmail.com>
2026-03-25 14:35:02 -0700

    Remove GA4 tracking functionality from the server-side API

--- nde-web/index.py
+++ nde-web/index.py
@@ -1,14 +1,12 @@
 import os
 
 from biothings.web.launcher import main
-from ga4 import ga4_log_function
 
 if __name__ == '__main__':
     SETTINGS = {
         "default_handler_class": 'handlers.WebAppHandler',
         "static_path": "dist/static",
         "template_path": os.path.dirname(__file__),
-        "log_function": ga4_log_function,
     }
     ROUTES = [
         (r" ^/$", "tornado.web.StaticFileHandler", {"path": "dist/static"}),
